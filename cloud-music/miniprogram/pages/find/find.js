2dd27a45656351753701464f315768b66e50e554
UnKn0W <yukikumo@outlook.com>
2021-01-26 17:10:13 +0800

    歌曲/歌单列表

---
+++ cloud-music/miniprogram/pages/find/find.js
@@ -0,0 +1,92 @@
+// pages/find/find.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    songs : [{
+      id : 1,
+      name : '他夏了夏天',
+      writer : '苏打绿-夏/狂热'
+    },
+    {
+      id : 2,
+      name : '橘子汽水',
+      writer : '南拳妈妈-优の良曲南搞小孩(新歌+精选)'
+    },
+    {
+      id : 3,
+      name : '夏天的味道',
+      writer : '陆虎-夏天的味道'
+    },
+    {
+      id : 4,
+      name : '一样的夏天',
+      writer : '孙燕姿-My Story,Your Song经典全纪录'
+    },
+    {
+      id : 5,
+      name : '他夏了夏天',
+      writer : '苏打绿-夏/狂热'
+    },
+    
+  ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ cloud-music/miniprogram/pages/music/music.js
@@ -0,0 +1,133 @@
+// pages/music/music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrl: [{
+        url: 'http://p1.music.126.net/zUv2mRobckK7Tdn2bp9iSA==/109951165664840470.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+      },
+      {
+        url: 'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+      },
+    ],
+    ssUrl: [{
+      picUrl: 'http://p3.music.126.net/qfKmb5grEFZNTGYKIONg1A==/109951165664253358.jpg?param=200y200',
+      count:990010000,
+      name : '[华语速爆新歌] 那英×姚晨首度联手合唱',
+      id : '2001',
+      },
+      {
+        picUrl: 'http://p3.music.126.net/RpzIkeUOZ4V7WBecytqH0Q==/109951165515305859.jpg?param=200y200',
+        count:100000,
+      name : '随情节流淌|富有叙事感的柔软音乐片段',
+      id : '2002',
+      },
+      {
+        picUrl: 'http://p4.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=200y200',
+        count:530000,
+      name : '流浪太空 | 宇宙永远是治愈一切的良药',
+      id : '2003',
+      },
+      {
+        picUrl: 'http://p4.music.126.net/1hzImIzOsRzG0iBSvHOO7w==/109951165511341127.jpg?param=200y200',
+        count:4665530000,
+      name : '今天从《Steady Me》听起|私人雷达',
+      id : '2004',
+      },
+      {
+        picUrl: 'http://p2.music.126.net/ySHtkTD4VuhQ3JDv4sreag==/109951165650282126.jpg?param=140y140',
+        count:140000,
+      name : '欧美小调° | 冬日舒适心情指南',
+      id : '2005',
+      },
+      {
+        picUrl: 'http://p4.music.126.net/AhgGd8XDHKm4HEywuLBp_A==/109951165662490579.jpg?param=200y200',
+        count:9000000,
+      name : '希望你的可爱，可以治愈一切烦恼',
+      id : '2006',
+      },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
