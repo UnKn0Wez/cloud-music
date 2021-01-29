2fa06031a7230ad0ed0bdff1eb7433b1594d25b9
UnKn0W <yukikumo@outlook.com>
2021-01-29 20:44:19 +0800

    播放详细页面的布局

--- cloud-music/miniprogram/pages/player/player.js
+++ cloud-music/miniprogram/pages/player/player.js
@@ -1,11 +1,14 @@
 // pages/player/player.js
+let musiclist=[]
+//用于记录当前播放歌曲index
+let playingIndex=0
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    picUrl:''
   },
 
   /**
@@ -13,13 +16,37 @@ Page({
    */
   onLoad: function (options) {
     // console.log(options)
+    // console.log(options.musicId,typeof(options.musicId))
+    playingIndex=options.index
+    musiclist=wx.getStorageSync('musiclist')
+    this._loadMusicDetail(options.musicId)
+  },
+
+  _loadMusicDetail(musicId){
+    let music=musiclist[playingIndex]
+    console.log(music)
+    wx.setNavigationBarTitle({
+      title: music.name,
+    })
+    this.setData({
+      picUrl:music.al.picUrl
+    })
+    wx.cloud.callFunction({
+      name:'music',
+      data:{
+        musicId,
+        $url:'musicUrl',
+      }
+    }).then((res)=>{
+      console.log(res)
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
